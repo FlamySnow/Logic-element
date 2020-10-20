77f7e4cbfa1540fb5db411e711fac3d32cf3032f
Anastasia <sweet.santonica@gmail.com>
2020-10-20 21:52:55 +0300

    Added comments

--- lib/logic_element.h
+++ lib/logic_element.h
@@ -3,7 +3,7 @@
 #include <iostream>
 
 namespace Program3 {
-    enum Type {input, output};
+    enum Type {input, output};//два типа клемм
 
     struct Clamp {
         int t;//тип - входная или выходная
@@ -18,19 +18,19 @@ namespace Program3 {
         static const int SZ = 10;//максимальное количество клемм
         int num;//количество клемм
         Clamp clmps[SZ];//массив описателей клемм
-        void corrSignal ();
-        void corrNumber (int n);
+        void corrSignal ();//проверка состояний сигналов на корректность
+        void corrNumber (int n);//проверка введённого номера клеммы
     public:
         LogElement();//пустой конструктор, лог. эл. - инвертор
         LogElement(int n, int m);//инициализация кол-вом входных и выходных клемм
         LogElement(int N);//инициализация массивом клемм заданной длины
-        friend std::istream & operator >> (std::istream &, LogElement &);
-        friend std::ostream & operator << (std::ostream &, const LogElement &);
-        void operator() (int n, char st);
-        char operator[] (int n);
-        int connect (int n);
-        int disconnect (int n);
-        LogElement & operator += (const Clamp &c);
+        friend std::istream & operator >> (std::istream &, LogElement &);//ввод состояний сигналов всех клемм
+        friend std::ostream & operator << (std::ostream &, const LogElement &);//вывод логического элемента в виде псевдографического рисунка
+        void operator() (int n, char st);//ввод состояния сигнала клеммы по заданному номеру
+        char operator[] (int n);//получение состояния сигнала клеммы по заданному номеру
+        int connect (int n);//увеличение соединений клеммы
+        int disconnect (int n);//уменьшение соединений клеммы
+        LogElement & operator += (const Clamp &c);//добавление клеммы к логическому элементу
     };
 }
 
